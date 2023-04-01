91bd8ac088e47795b096ac4ae9030166775ba060
oliviafan0703 <zf59@duke.edu>
2023-04-01 16:28:44 -0400

    pushing

---
+++ benchmarks/genData.py
@@ -0,0 +1,21 @@
+import numpy as np
+import h5py
+
+numPts = 20
+numDims = 3
+numK = 4
+filename = "test1.h5"
+
+means = 200 * np.random.rand(numK, numDims) - 100
+x=[]
+for k in range(numK):
+	x.append( means[k] +  np.random.randn(numPts, numDims) )
+x = np.vstack(x)
+np.random.shuffle(x)
+
+with h5py.File(filename, "w") as data_file:
+    data_file.create_dataset("dataset_name", data=x, dtype='float32')
+
+print(x.shape)
+
+
---
+++ benchmarks/view.py
@@ -0,0 +1,27 @@
+import h5py
+
+
+filename = 'test1.h5'
+
+with h5py.File(filename, 'r') as f:
+    print("Keys: %s" % list(f.keys())[0])
+    a_group_key = list(f.keys())[0]
+
+    # get the object type for a_group_key: usually group or dataset
+    print(type(f[a_group_key])) 
+
+    # If a_group_key is a group name, 
+    # this gets the object names in the group and returns as a list
+    data = list(f[a_group_key])
+
+    # preferred methods to get dataset values:
+    ds_obj = f[a_group_key]      # returns as a h5py dataset object
+    ds_arr = f[a_group_key][()]  # returns as a numpy array
+
+    # get the shape of the dataset
+    print(ds_obj.shape)
+    print(data)
+
+
+
+
